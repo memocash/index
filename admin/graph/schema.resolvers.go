f6d73e8c98cb1fbc246d263b1ba799801a9c091d
Jason Chavannes <jason.chavannes@gmail.com>
2021-10-27 20:55:22 +0000

    Only retrive graphql fields if requested

--- admin/graph/schema.resolvers.go
+++ admin/graph/schema.resolvers.go
@@ -6,6 +6,8 @@ package graph
 import (
 	"context"
 	"fmt"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/jchavannes/jgo/jutil"
 
 	"github.com/memocash/server/admin/graph/generated"
 	"github.com/memocash/server/admin/graph/model"
@@ -20,9 +22,17 @@ func (r *queryResolver) Tx(ctx context.Context, hash *string) (*model.Tx, error)
 	if hash != nil {
 		hashString += " - " + *hash
 	}
+	preloads := GetPreloads(ctx)
+	fmt.Printf("preloads: %#v\n", preloads)
+	var raw string
+	if jutil.StringInSlice("raw", preloads) {
+		raw = "456"
+	} else {
+		fmt.Println("raw not requested")
+	}
 	return &model.Tx{
 		Hash: hashString,
-		Raw:  "456",
+		Raw:  raw,
 	}, nil
 }
 
@@ -41,3 +51,27 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+func GetPreloads(ctx context.Context) []string {
+	return GetNestedPreloads(
+		graphql.GetOperationContext(ctx),
+		graphql.CollectFieldsCtx(ctx, nil),
+		"",
+	)
+}
+
+func GetNestedPreloads(ctx *graphql.OperationContext, fields []graphql.CollectedField, prefix string) (preloads []string) {
+	for _, column := range fields {
+		prefixColumn := GetPreloadString(prefix, column.Name)
+		preloads = append(preloads, prefixColumn)
+		preloads = append(preloads, GetNestedPreloads(ctx, graphql.CollectFields(ctx, column.Selections, nil), prefixColumn)...)
+	}
+	return
+}
+
+func GetPreloadString(prefix, name string) string {
+	if len(prefix) > 0 {
+		return prefix + "." + name
+	}
+	return name
+}
--- admin/server/graphql/main.go
+++ admin/server/graphql/main.go
@@ -11,24 +11,3 @@ import (
 func GetHandler() (http.Handler, error) {
 	return handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})), nil
 }
-
-/*func GetHandler() (http.Handler, error) {
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		return nil, jerr.Get("error getting graphql schema", err)
-	}
-	return handler.New(&handler.Config{
-		Schema: &schema,
-		Pretty: true,
-	}), nil
-}*/
