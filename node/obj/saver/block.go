1bd2716d53f9bb1d16b35f43df4794d21c721886
Jason Chavannes <jason.chavannes@gmail.com>
2021-11-14 17:44:22 -0800

    Remove duplicate logging

--- node/obj/saver/block.go
+++ node/obj/saver/block.go
@@ -21,7 +21,6 @@ type Block struct {
 }
 
 func (t *Block) SaveBlock(header wire.BlockHeader) error {
-	t.Verbose = true
 	t.BlockHash = header.BlockHash()
 	t.BlockHashBytes = t.BlockHash.CloneBytes()
 	err := t.saveBlockObjects(header)
