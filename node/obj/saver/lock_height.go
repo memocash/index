192ea67aa475f10b58d016aad58c65e421f01c3e
Jason Chavannes <jason.chavannes@gmail.com>
2022-02-14 23:12:50 -0800

    Save lock height output inputs for outputs

--- node/obj/saver/lock_height.go
+++ node/obj/saver/lock_height.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jchavannes/btcd/wire"
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/jchavannes/jgo/jlog"
+	"github.com/jchavannes/jgo/jutil"
 	"github.com/memocash/index/db/item"
 	"github.com/memocash/index/ref/bitcoin/memo"
 	"github.com/memocash/index/ref/bitcoin/tx/script"
@@ -46,6 +47,7 @@ func (t *LockHeight) QueueTxs(block *wire.MsgBlock) error {
 	var objects []item.Object
 	var lockHeightOutputsToRemove []*item.LockHeightOutput
 	var objectCount int
+	var outsToQueryInputs []memo.Out
 	for _, tx := range block.Transactions {
 		txHash := tx.TxHash()
 		txHashBytes := txHash.CloneBytes()
@@ -53,18 +55,24 @@ func (t *LockHeight) QueueTxs(block *wire.MsgBlock) error {
 			jlog.Logf("tx: %s\n", txHash.String())
 		}
 		for h := range tx.TxOut {
+			var index = uint32(h)
 			lockHash := script.GetLockHash(tx.TxOut[h].PkScript)
 			var lockHeightOutput = &item.LockHeightOutput{
 				LockHash: lockHash,
 				Height:   height,
 				Hash:     txHashBytes,
-				Index:    uint32(h),
+				Index:    index,
 			}
+			outsToQueryInputs = append(outsToQueryInputs, memo.Out{
+				LockHash: lockHash,
+				TxHash:   txHashBytes,
+				Index:    index,
+			})
 			if height > 0 {
 				lockHeightOutputsToRemove = append(lockHeightOutputsToRemove, &item.LockHeightOutput{
 					LockHash: lockHash,
 					Hash:     txHashBytes,
-					Index:    uint32(h),
+					Index:    index,
 				})
 			}
 			objects = append(objects, lockHeightOutput)
@@ -93,7 +101,7 @@ func (t *LockHeight) QueueTxs(block *wire.MsgBlock) error {
 		}
 	}
 	if err := item.Save(objects); err != nil {
-		return jerr.Get("error saving db lock height objects", err)
+		return jerr.Get("error saving db lock height objects 1", err)
 	}
 	objects = nil
 	outputs, err := item.GetTxOutputs(outputsToGet)
@@ -122,8 +130,62 @@ func (t *LockHeight) QueueTxs(block *wire.MsgBlock) error {
 			})
 		}
 	}
+	outputInputs, err := item.GetOutputInputs(outsToQueryInputs)
+	if err != nil {
+		return jerr.Get("error getting output inputs for lock output inputs", err)
+	}
+	var txHashes = make([][]byte, len(outputInputs))
+	for i := range outputInputs {
+		txHashes[i] = outputInputs[i].Hash
+	}
+	txHashes = jutil.RemoveDupesAndEmpties(txHashes)
+	txBlocks, err := item.GetTxBlocks(txHashes)
+	if err != nil {
+		return jerr.Get("error getting tx blocks for lock height output inputs", err)
+	}
+	var blockHashesToGetHeights = make([][]byte, len(txBlocks))
+	for i := range txBlocks {
+		blockHashesToGetHeights[i] = txBlocks[i].BlockHash
+	}
+	blockHeights, err := item.GetBlockHeights(blockHashesToGetHeights)
+	if err != nil {
+		return jerr.Get("error getting block heights for lock height output inputs", err)
+	}
+	for _, outputInput := range outputInputs {
+		var lockHash []byte
+		for _, outsToQueryInput := range outsToQueryInputs {
+			if bytes.Equal(outsToQueryInput.TxHash, outputInput.PrevHash) &&
+				outsToQueryInput.Index == outputInput.PrevIndex {
+				lockHash = outsToQueryInput.LockHash
+				break
+			}
+		}
+		var txBlockHash []byte
+		for _, txBlock := range txBlocks {
+			if bytes.Equal(txBlock.TxHash, outputInput.Hash) {
+				txBlockHash = txBlock.BlockHash
+				break
+			}
+		}
+		var txBlockHeight int64
+		if len(txBlockHash) > 0 {
+			for _, blockHeight := range blockHeights {
+				if bytes.Equal(blockHeight.BlockHash, txBlockHash) {
+					txBlockHeight = blockHeight.Height
+				}
+			}
+		}
+		objects = append(objects, &item.LockHeightOutputInput{
+			LockHash:  lockHash,
+			Height:    txBlockHeight,
+			Hash:      outputInput.Hash,
+			Index:     outputInput.Index,
+			PrevHash:  outputInput.PrevHash,
+			PrevIndex: outputInput.PrevIndex,
+		})
+	}
 	if err := item.Save(objects); err != nil {
-		return jerr.Get("error saving db lock height objects", err)
+		return jerr.Get("error saving db lock height objects 2", err)
 	}
 	if err := item.RemoveLockHeightOutputs(lockHeightOutputsToRemove); err != nil {
 		return jerr.Get("error removing mempool lock height outputs for lock heights", err)
