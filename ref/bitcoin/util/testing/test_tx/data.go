b1a91f8897dc9f9302f6ffa68037366301e9db35
Jason Chavannes <jason.chavannes@gmail.com>
2022-05-24 22:55:46 -0700

    Test tx blocks

--- ref/bitcoin/util/testing/test_tx/data.go
+++ ref/bitcoin/util/testing/test_tx/data.go
@@ -2,6 +2,7 @@ package test_tx
 
 import (
 	"encoding/hex"
+	"github.com/jchavannes/btcd/chaincfg/chainhash"
 	"github.com/memocash/index/ref/bitcoin/memo"
 	"github.com/memocash/index/ref/bitcoin/tx/build"
 	"github.com/memocash/index/ref/bitcoin/tx/gen"
@@ -139,8 +140,42 @@ var (
 	GenericTxHash7 = GetHashBytes(GenericTxHashString7)
 	GenericTxHash8 = GetHashBytes(GenericTxHashString8)
 	GenericTxHash9 = GetHashBytes(GenericTxHashString9)
+)
 
-	Block1Header = GetBlockHeader(Block1Raw)
+const (
+	Block659588HashStr       = "00000000000000000177033d90e3c3622d14b1734c01bd6a29b3f93739770ee5"
+	Block659589OrphanHashStr = "000000000000000002a705c477b54cd00bd7c2e89fb23e19a5d1d783a01d8eeb"
+	Block659589RealHashStr   = "000000000000000003802538273ed0e9dbffeddbebf1ad3d4f8fed673843f811"
+	Block659590HashStr       = "0000000000000000012885749aaa6080113077df2e65306396beee2037595b2e"
+	Block659591HashStr       = "000000000000000000b277519a5b63eb3074d4de80f56d88baf042ebd4268195"
+	Block659592HashStr       = "000000000000000000cd175279d91591109eab76f496dc611fafb04e39f42f1f"
+	Block659593HashStr       = "000000000000000003a970548dffc956268d0f4365eb44dee2a516e71114551e"
+
+	Block659588HeaderStr       = "00e0ff27467538273c3d2a16a09b12ebb3f76fb1126909df199210000000000000000000bcab521d0b16f9a30cef1f941d7a76d31aa33355bf29b381f41b49b7a2d960ee122f9e5fc5c3031842504572"
+	Block659589OrphanHeaderStr = "00e00020e50e773937f9b3296abd014c73b1142d62c3e3903d037701000000000000000032e0839abee92ea103447cee858d6a689f40e564d15898a4fcdf68f2c050088f962f9e5fdbc20318670544a9"
+	Block659589RealHeaderStr   = "00c0ff3fe50e773937f9b3296abd014c73b1142d62c3e3903d037701000000000000000039438fb6ad5fc772c46ea72d1a29b27ec334bb599a25c129be113d9d52000f03652f9e5fdbc2031801585a65"
+	Block659590HeaderStr       = "0000402011f8433867ed8f4f3dadf1ebdbedffdbe9d03e27382580030000000000000000096e275c225d97b54301b9b5ee3e3db10aa92b6d87a0d6db251051d1a9648c23df309e5fabbd031860e8765a"
+	Block659591HeaderStr       = "000000202e5b593720eebe966330652edf7730118060aa9a748528010000000000000000b68e1e78d69878b0311df7919786c18b6b62a84b2ff39f4ae587d2724b7faceb7e319e5f0bc10318107d5762"
+	Block659592HeaderStr       = "00008020958126d4eb42f0ba886df580ded47430eb635b9a5177b200000000000000000012347e7536122e71474918a2d05e4aa234dc61409ecdb138aba0865941e8b00f95319e5f74b803180deccfd1"
+	Block659593HeaderStr       = "000000201f2ff4394eb0af1f61dc96f476ab9e109115d9795217cd00000000000000000057dcdb4c8ae351a95f550d09b4ea53c4375d3bcdbbd6d3a9137b7eb481dfe9012b329e5f19bc031832157b96"
+)
+
+var (
+	Block659588Hash, _       = chainhash.NewHashFromStr(Block659588HashStr)
+	Block659589OrphanHash, _ = chainhash.NewHashFromStr(Block659589OrphanHashStr)
+	Block659589RealHash, _   = chainhash.NewHashFromStr(Block659589RealHashStr)
+	Block659590Hash, _       = chainhash.NewHashFromStr(Block659590HashStr)
+	Block659591Hash, _       = chainhash.NewHashFromStr(Block659591HashStr)
+	Block659592Hash, _       = chainhash.NewHashFromStr(Block659592HashStr)
+	Block659593Hash, _       = chainhash.NewHashFromStr(Block659593HashStr)
+
+	Block659588Header       = GetBlockHeader(Block659588HeaderStr)
+	Block659589OrphanHeader = GetBlockHeader(Block659589OrphanHeaderStr)
+	Block659589RealHeader   = GetBlockHeader(Block659589RealHeaderStr)
+	Block659590Header       = GetBlockHeader(Block659590HeaderStr)
+	Block659591Header       = GetBlockHeader(Block659591HeaderStr)
+	Block659592Header       = GetBlockHeader(Block659592HeaderStr)
+	Block659593Header       = GetBlockHeader(Block659593HeaderStr)
 )
 
 func GetAddress1WalletSingle100k() build.Wallet {
