36100190371f52c51d8e772ee8e89299b3857cff
Jason Chavannes <jason.chavannes@gmail.com>
2022-08-25 21:34:12 -0700

    Increase compaction total size too

--- db/store/main.go
+++ db/store/main.go
@@ -41,6 +41,7 @@ func getDb(topic string, shard uint) (*leveldb.DB, error) {
 		db, err := leveldb.OpenFile(filename, &opt.Options{
 			OpenFilesCacheCapacity: openFilesCacheCapacity,
 			CompactionTableSize:    compactionDataSize * opt.MiB,
+			CompactionTotalSize:    compactionDataSize * 5 * opt.MiB,
 		})
 		if err != nil {
 			return nil, jerr.Get("error opening level db", err)
