a82d8d899c6d287235ca260265d680d77ffad6d4
Jason Chavannes <jason.chavannes@gmail.com>
2022-10-13 15:36:32 -0700

    If broadcast config is not set, just send to network

--- ref/broadcast/broadcast_client/broadcast.go
+++ ref/broadcast/broadcast_client/broadcast.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/memocash/index/ref/broadcast/gen/broadcast_pb"
 	"github.com/memocash/index/ref/config"
+	"github.com/memocash/index/ref/network/network_client"
 )
 
 type Broadcast struct {
@@ -14,6 +15,9 @@ func (t *Broadcast) Broadcast(ctx context.Context, raw []byte) error {
 	conn, err := NewConnection()
 	if err != nil {
 		if config.IsConfigNotSetError(err) {
+			if err := network_client.NewSendTx().Send([][]byte{raw}); err != nil {
+				return jerr.Get("error sending raw txs to network", err)
+			}
 			return nil
 		}
 		return jerr.Get("error connecting to broadcast", err)
