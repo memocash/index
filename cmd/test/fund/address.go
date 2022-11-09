8496e51c1fe2d0ed1c24d7c81e746b7fd6a2ec47
Jason Chavannes <jason.chavannes@gmail.com>
2022-11-09 12:33:43 -0800

    Get funding tx to work

--- cmd/test/fund/address.go
+++ cmd/test/fund/address.go
@@ -1,46 +1,37 @@
 package fund
 
 import (
+	"github.com/jchavannes/btcd/wire"
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/jchavannes/jgo/jutil"
 	"github.com/memocash/index/node/obj/saver"
 	"github.com/memocash/index/ref/bitcoin/memo"
+	"github.com/memocash/index/ref/bitcoin/tx/parse"
 	"github.com/memocash/index/ref/bitcoin/util/testing/test_tx"
 	"github.com/memocash/index/ref/bitcoin/wallet"
 	"github.com/memocash/index/ref/dbi"
-	"github.com/memocash/index/test/grp"
 	"github.com/spf13/cobra"
 )
 
 var addressCmd = &cobra.Command{
 	Use: "address",
-	Run: func(c *cobra.Command, args []string){
-		s := grp.DoubleSpend{
-			TxSaver:         nil,
-			DelayedTxSaver:  nil,
-			DelayAmount:     0,
-			BlockSaver:      nil,
-			FundingPkScript: nil,
-			OldBlocks:       nil,
-		}
-		s.TxSaver = saver.NewCombined([]dbi.TxSave{
+	Run: func(c *cobra.Command, args []string) {
+		txSaver := saver.NewCombined([]dbi.TxSave{
 			saver.NewTxRaw(false),
 			saver.NewTx(false),
 			saver.NewUtxo(false),
 			saver.NewLockHeight(false),
-			saver.NewDoubleSpend(false),
 		})
-		s.BlockSaver = saver.BlockSaver(false)
-
 		address := wallet.GetAddressFromString(args[0])
 		amount := jutil.GetInt64FromString(args[1])
-		fundingTx,err := test_tx.GetFundingTx(address, amount)
+		fundingTx, err := test_tx.GetFundingTx(address, amount)
 		if err != nil {
 			jerr.Get("error getting funding tx for addressCmd", err).Fatal()
 		}
-		if err := s.SaveBlock([]*memo.Tx{fundingTx}); err != nil {
+		txInfo := parse.GetTxInfo(fundingTx)
+		txInfo.Print()
+		if err := txSaver.SaveTxs(memo.GetBlockFromTxs([]*wire.MsgTx{fundingTx.MsgTx}, nil)); err != nil {
 			jerr.Get("error saving funding tx", err).Fatal()
 		}
 	},
 }
-
