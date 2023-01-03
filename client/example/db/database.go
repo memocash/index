35aedb5e75b3421dad8b2a3f91d80bca11dc1bd0
Jason Chavannes <jason.chavannes@gmail.com>
2023-01-03 11:38:35 -0800

    Fix query results

--- client/example/db/database.go
+++ client/example/db/database.go
@@ -35,8 +35,6 @@ func (d *Database) GetAddressBalance(address *wallet.Addr) (int64, error) {
 	query := "" +
 		"SELECT " +
 		"   outputs.address, " +
-		"   COUNT(DISTINCT (outputs.hash || outputs.`index`)) AS output_count, " +
-		"   IFNULL(SUM(CASE WHEN inputs.hash IS NULL THEN 1 ELSE 0 END), 0) AS utxo_count, " +
 		"   IFNULL(SUM(CASE WHEN inputs.hash IS NULL THEN outputs.value ELSE 0 END), 0) AS balance " +
 		"FROM outputs " +
 		"LEFT JOIN inputs ON (inputs.prev_hash = outputs.hash AND inputs.prev_index = outputs.`index`) " +
@@ -46,7 +44,7 @@ func (d *Database) GetAddressBalance(address *wallet.Addr) (int64, error) {
 		Address string
 		Balance int64
 	}
-	if err := d.Db.QueryRow(query, address.String()).Scan(&result); err != nil {
+	if err := d.Db.QueryRow(query, address.String()).Scan(&result.Address, &result.Balance); err != nil {
 		return 0, jerr.Get("error getting address balance exec query", err)
 	}
 	return result.Balance, nil
@@ -63,16 +61,16 @@ func (d *Database) GetAddressHeight(address *wallet.Addr) (int64, error) {
 		"WHERE outputs.address = ? " +
 		"GROUP BY outputs.address "
 	var result struct {
-		Address string
-		Height  int64
+		Address string `db:"address"`
+		Height  int64  `db:"height"`
 	}
-	if err := d.Db.QueryRow(query, address.String()).Scan(&result); err != nil {
-		return 0, jerr.Get("error getting address balance exec query", err)
+	if err := d.Db.QueryRow(query, address.String()).Scan(&result.Address, &result.Height); err != nil {
+		return 0, jerr.Get("error getting address height exec query", err)
 	}
 	return result.Height, nil
 }
 
-func (d * Database) GetUtxos(address *wallet.Addr) ([]graph.Output, error){
+func (d *Database) GetUtxos(address *wallet.Addr) ([]graph.Output, error) {
 	query := "" +
 		"SELECT outputs.* FROM outputs " +
 		"LEFT JOIN inputs ON (inputs.prev_hash = outputs.hash AND inputs.prev_index = outputs.`index`) " +
@@ -117,7 +115,7 @@ func (d *Database) SaveTxs(txs []graph.Tx) error {
 		for _, block := range tx.Blocks {
 			queries = append(queries, Query{
 				Name:      "blocks",
-				Query:     "INSERT OR IGNORE INTO blocks (hash, timestamp, height) VALUES (?, ?)",
+				Query:     "INSERT OR IGNORE INTO blocks (hash, timestamp, height) VALUES (?, ?, ?)",
 				Variables: []interface{}{block.Hash, block.Timestamp.Format(time.RFC3339Nano), block.Height},
 			}, Query{
 				Name:      "block_txs",
