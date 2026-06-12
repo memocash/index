b4d8984aed5fb06c030299918366030a9168f6c2
Jason Chavannes <jason.chavannes@gmail.com>
2026-06-12 13:03:31 -0700

    Use multi-hash block locators when requesting headers so the peer can find a common block after a chain reorg instead of falling back to genesis. Add per-header continuity and reorg handling to ScanHeaders so it no longer blindly increments height, instead replacing orphaned branches and rolling back sync status.

--- ref/cluster/lead/scan_headers.go
+++ ref/cluster/lead/scan_headers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/jchavannes/btcd/peer"
 	"github.com/jchavannes/btcd/wire"
 	"github.com/jchavannes/jgo/jfmt"
+	"github.com/memocash/index/db/client"
+	"github.com/memocash/index/db/item"
 	"github.com/memocash/index/db/item/chain"
 	"github.com/memocash/index/db/item/db"
 	"github.com/memocash/index/node/conn"
@@ -19,6 +21,7 @@ import (
 type ScanHeaders struct {
 	height    int64
 	startHash *chainhash.Hash
+	lastHash  *chainhash.Hash
 	peer      *peer.Peer
 	Rescan    bool
 }
@@ -33,6 +36,7 @@ func (s *ScanHeaders) Run() error {
 			s.height = recentBlock.Height
 			blockHash := chainhash.Hash(recentBlock.BlockHash)
 			s.startHash = &blockHash
+			s.lastHash = &blockHash
 			log.Printf("ScanHeaders resuming from height: %s\n", jfmt.AddCommas(s.height))
 		}
 	}
@@ -53,15 +57,11 @@ func (s *ScanHeaders) Run() error {
 }
 
 func (s *ScanHeaders) OnVerAck(_ *peer.Peer, _ *wire.MsgVerAck) {
-	var locatorHash *chainhash.Hash
-	if s.startHash != nil {
-		locatorHash = s.startHash
-	} else {
-		locatorHash = wallet.GetGenesisBlock().Hash
+	if s.startHash == nil {
+		s.requestHeadersFrom(wallet.GetGenesisBlock().Hash)
+		return
 	}
-	msgGetHeaders := wire.NewMsgGetHeaders()
-	msgGetHeaders.BlockLocatorHashes = append(msgGetHeaders.BlockLocatorHashes, locatorHash)
-	s.peer.QueueMessage(msgGetHeaders, nil)
+	s.requestLocatorHeaders()
 }
 
 func (s *ScanHeaders) OnHeaders(_ *peer.Peer, msg *wire.MsgHeaders) {
@@ -69,10 +69,37 @@ func (s *ScanHeaders) OnHeaders(_ *peer.Peer, msg *wire.MsgHeaders) {
 		s.peer.Disconnect()
 		return
 	}
+	ctx := context.TODO()
 	var objects []db.Object
 	for _, blockHeader := range msg.Headers {
-		s.height++
 		blockHash := blockHeader.BlockHash()
+		if s.lastHash != nil && blockHeader.PrevBlock == *s.lastHash {
+			s.height++
+		} else {
+			height, ok, err := s.resolveHeight(ctx, blockHeader.PrevBlock)
+			if err != nil {
+				log.Fatalf("error determining header height; %v", err)
+			}
+			if !ok {
+				log.Printf("ScanHeaders header %s does not connect (prev %s); re-requesting from height %s\n",
+					blockHash, blockHeader.PrevBlock, jfmt.AddCommas(s.height))
+				if err := s.saveHeaders(objects); err != nil {
+					log.Fatalf("error saving header objects; %v", err)
+				}
+				s.requestLocatorHeaders()
+				return
+			}
+			reorged, err := s.clearReorg(ctx, height, blockHash)
+			if err != nil {
+				log.Fatalf("error clearing reorged height index at height %d; %v", height, err)
+			}
+			if reorged {
+				if err := rollBackSyncStatus(ctx, height-1); err != nil {
+					log.Fatalf("error rolling back sync status; %v", err)
+				}
+			}
+			s.height = height
+		}
 		headerRaw := memo.GetRawBlockHeader(*blockHeader)
 		objects = append(objects,
 			&chain.Block{
@@ -88,18 +115,125 @@ func (s *ScanHeaders) OnHeaders(_ *peer.Peer, msg *wire.MsgHeaders) {
 				BlockHash: blockHash,
 			},
 		)
+		lastHash := blockHash
+		s.lastHash = &lastHash
 	}
-	if err := db.Save(objects); err != nil {
+	if err := s.saveHeaders(objects); err != nil {
 		log.Fatalf("error saving header objects; %v", err)
 	}
 	if s.height%2000 == 0 {
 		log.Printf("Scanned headers to height %s\n", jfmt.AddCommas(s.height))
 	}
-	lastHeader := msg.Headers[len(msg.Headers)-1]
-	lastHash := lastHeader.BlockHash()
+	s.requestHeadersFrom(s.lastHash)
+}
+
+func (s *ScanHeaders) saveHeaders(objects []db.Object) error {
+	if len(objects) == 0 {
+		return nil
+	}
+	if err := db.Save(objects); err != nil {
+		return fmt.Errorf("error saving header objects; %w", err)
+	}
+	return nil
+}
+
+func (s *ScanHeaders) resolveHeight(ctx context.Context, prevBlock chainhash.Hash) (int64, bool, error) {
+	if prevBlock == *wallet.GetGenesisBlock().Hash {
+		return 1, true, nil
+	}
+	blockHeight, err := chain.GetBlockHeight(ctx, prevBlock)
+	if err != nil {
+		if client.IsEntryNotFoundError(err) {
+			return 0, false, nil
+		}
+		return 0, false, fmt.Errorf("error getting previous block height; %w", err)
+	}
+	return blockHeight.Height + 1, true, nil
+}
+
+func (s *ScanHeaders) clearReorg(ctx context.Context, height int64, newHash chainhash.Hash) (bool, error) {
+	existing, err := chain.GetHeightBlock(ctx, height)
+	if err != nil {
+		return false, fmt.Errorf("error getting height block; %w", err)
+	}
+	var isReorg bool
+	for _, hb := range existing {
+		if chainhash.Hash(hb.BlockHash) != newHash {
+			isReorg = true
+			break
+		}
+	}
+	if !isReorg {
+		return false, nil
+	}
+	log.Printf("ScanHeaders reorg at height %s: replacing orphaned branch with %s\n",
+		jfmt.AddCommas(height), newHash)
+	nextHeight := height
+	for {
+		heightBlocks, err := chain.GetHeightBlocksAllLimit(ctx, nextHeight, client.HugeLimit, false)
+		if err != nil {
+			return false, fmt.Errorf("error getting orphaned height blocks; %w", err)
+		}
+		if len(heightBlocks) == 0 {
+			break
+		}
+		var objects []db.Object
+		for _, hb := range heightBlocks {
+			objects = append(objects,
+				&chain.HeightBlock{Height: hb.Height, BlockHash: hb.BlockHash},
+				&chain.BlockHeight{Height: hb.Height, BlockHash: hb.BlockHash},
+			)
+			nextHeight = hb.Height + 1
+		}
+		if err := db.Remove(objects); err != nil {
+			return false, fmt.Errorf("error removing orphaned height index; %w", err)
+		}
+	}
+	return true, nil
+}
+
+func (s *ScanHeaders) requestHeadersFrom(locatorHash *chainhash.Hash) {
+	if locatorHash == nil {
+		locatorHash = wallet.GetGenesisBlock().Hash
+	}
 	msgGetHeaders := wire.NewMsgGetHeaders()
-	msgGetHeaders.BlockLocatorHashes = append(msgGetHeaders.BlockLocatorHashes, &lastHash)
+	msgGetHeaders.BlockLocatorHashes = append(msgGetHeaders.BlockLocatorHashes, locatorHash)
 	s.peer.QueueMessage(msgGetHeaders, nil)
+}
+
+func (s *ScanHeaders) requestLocatorHeaders() {
+	locatorHashes, err := getBlockLocator(context.TODO(), s.height)
+	if err != nil {
+		log.Fatalf("error building block locator for scan headers (height: %d); %v", s.height, err)
+	}
+	msgGetHeaders := wire.NewMsgGetHeaders()
+	msgGetHeaders.BlockLocatorHashes = locatorHashes
+	s.peer.QueueMessage(msgGetHeaders, nil)
+}
+
+func rollBackSyncStatus(ctx context.Context, height int64) error {
+	if height < 0 {
+		height = 0
+	}
+	syncStatus, err := item.GetSyncStatus(ctx, item.SyncStatusBlockHeight)
+	if err != nil {
+		if client.IsEntryNotFoundError(err) {
+			return nil
+		}
+		return fmt.Errorf("error getting sync status; %w", err)
+	}
+	if syncStatus.Height <= height {
+		return nil
+	}
+	log.Printf("ScanHeaders rolling sync status back from %s to %s due to reorg\n",
+		jfmt.AddCommas(syncStatus.Height), jfmt.AddCommas(height))
+	if err := db.Save([]db.Object{&item.SyncStatus{
+		Name:   item.SyncStatusBlockHeight,
+		Height: height,
+	}}); err != nil {
+		return fmt.Errorf("error saving sync status; %w", err)
+	}
+	return nil
 }
 
 func (s *ScanHeaders) OnVersion(_ *peer.Peer, msg *wire.MsgVersion) {
